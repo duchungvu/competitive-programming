f5b266859f8c9376710b0fee34facea8eb0f9eb1
Hung Vu <duchungvu1999@gmail.com>
2020-02-25 12:15:40 -0500

    G O(logDn)

--- ICPC/NAC20/G.cpp
+++ ICPC/NAC20/G.cpp
@@ -3,36 +3,28 @@ using namespace std;
 
 int n, p, q, s;
 int a[200005], b[200005];
+int pre[200005], nxt[200005];
 
-bool check(int x) {
-    // cout << "check: " << x << endl;
-    bool check[200005];
-    memset(check, false, sizeof(check));
-    int r = q - 1;
-    while (r >= 0 && b[r] - a[p - 1] > x) r--;
-    int l = r;
-    while (l >= 0 && a[p - 1] - b[l] <= x) l--;
-    int k = 0;
-    while (k <= q - 1 && a[p - 1] + b[k] <= s) k++;
-    int cur = 0;
-    for (int i = p - 1; i >= 0; i--) {
-        while (r >= 0 && b[r] - a[i] > x) r--;
-        while (l >= 0 && a[i] - b[l] <= x) l--;
-        while (k <= q - 1 && a[i] + b[k] <= s) k++;
-        // cout << a[i] << ' ' << l << ' ' << r << ' ' << k << ' ' << endl;
-        for (int j = min(r, k - 1); j > l; j--) {
-            if (!check[j]) {
-                check[j] = true;
-                cur++;
-                break;
-            }
-        }
-        if (cur >= n) return true;
+bool check(int d) {
+    for (int i = 0; i < q + 2; i++) {
+        pre[i] = i - 1;
+        nxt[i] = i + 1;
     }
-    // for (int i = 0; i < q; i++) {
-    //     cout << check[i] << " ";
-    // }
-    // cout << endl;
+    int cur = 0, lowD = q, highD = q, highS = q;
+    while (highS >= 0 && a[p - 1] + b[highS] > s) highS = pre[highS];
+    for (int i = p - 1; i >= 0; i--) {
+        while (lowD > 0 && a[i] - b[lowD] <= d) lowD = pre[lowD];
+        while (highD > 0 && b[highD] - a[i] > d) highD = pre[highD];
+        while (highS <= q && a[i] + b[highS] <= s) highS = nxt[highS];
+        int l = nxt[lowD], r = min(highD, pre[highS]);
+        if (r < l) continue; 
+        if (lowD == r) lowD = pre[lowD];
+        if (highD == r) highD = pre[highD];
+        if (highS == r) highS = nxt[highS];
+        nxt[pre[r]] = nxt[r];
+        pre[nxt[r]] = pre[r];
+        if (++cur >= n) return true;
+    }
     return false;
 }
 
@@ -41,15 +33,15 @@ int main() {
     for (int i = 0; i < p; i++) {
         cin >> a[i];
     }
-    for (int i = 0; i < q; i++) {
+    for (int i = 1; i < q + 1; i++) {
         cin >> b[i];
     }
     sort(a, a + p);
-    sort(b, b + q);
+    sort(b, b + 1 + q);
 
     int res = -1;
     int left = 0;
-    int right = max(a[p - 1] - b[0], b[q - 1] - a[0]);
+    int right = max(a[p - 1] - b[0], b[q] - a[0]);
     while (left <= right) {
         int mid = (left + right) / 2;
         if (check(mid)) {
